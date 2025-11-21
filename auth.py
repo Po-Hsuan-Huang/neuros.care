1b26ecbb23bc059c364792a3f07d3b8f441fd2f7
po-hsuan-huang <lets.lincredible@gmail.com>
2025-11-21 02:26:52 -0500

    auth.py

--- auth.py
+++ auth.py
@@ -62,7 +62,7 @@ def welcome():
         from flask import session
         session["username"] = name
         # Redirect to React app entry point
-        return redirect(url_for("http://dev.neuros.care"))
+        return redirect("http://dev.neuros.care")
     
     return "Failed to fetch user data.", 500
 
